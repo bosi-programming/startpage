b46c6c498bd621b7ffcf29383fad1d8765bae4e2
Felipe Azevedo Bosi <bosiarquitetura@gmail.com>
2022-11-17 10:39:52 -0300

    Change port because of Mac

--- rollup.config.js
+++ rollup.config.js
@@ -19,7 +19,7 @@ function serve() {
       if (server) return;
       server = require("child_process").spawn(
         "npm",
-        ["run", "start", "--", "--dev"],
+        ["run", "start", "--", "--dev", "--port=8080"],
         {
           stdio: ["ignore", "inherit", "inherit"],
           shell: true,
