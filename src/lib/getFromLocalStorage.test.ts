1c2428fb73187e798dbb4144f4f1b445e5271141
Felipe Bosi <contato@felipebosi.com>
2025-03-13 09:22:05 -0300

    Add first test to app

---
+++ src/lib/getFromLocalStorage.test.ts
@@ -0,0 +1,31 @@
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import { getFromLocalStorage } from './getFromLocalStorage'
+import * as env from '$app/environment';
+
+const fakeLocalStorage = { test: '{"test": "test"}', asString: 'test' };
+
+const fakeLocalStorageAPI = {
+  getItem: (localStorageName: keyof typeof fakeLocalStorage) => fakeLocalStorage[localStorageName]
+}
+
+vi.stubGlobal('localStorage', fakeLocalStorageAPI)
+
+describe('getFromLocalStorage', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+  it('Should get an item from localStorage', () => {
+    vi.spyOn(env, 'browser', 'get').mockReturnValue(true)
+    const item = getFromLocalStorage('test');
+    expect(item).toStrictEqual({ test: "test" });
+  })
+  it('Should return item as string if !object', () => {
+    vi.spyOn(env, 'browser', 'get').mockReturnValue(true)
+    const item = getFromLocalStorage('asString');
+    expect(item).toBe('test');
+  })
+  it('Should not return item if !browser', () => {
+    const item = getFromLocalStorage('test');
+    expect(item).toBe(undefined);
+  })
+})
--- src/lib/getFromLocalStorage.ts
+++ src/lib/getFromLocalStorage.ts
@@ -1,10 +1,14 @@
 import { browser } from "$app/environment";
 
 export const getFromLocalStorage = (localStorageName: string) => {
-  if(browser) {
-  const localStorageItem = localStorage.getItem(localStorageName);
-  if (localStorageItem && localStorageItem !== 'undefined') {
-    return JSON.parse(localStorageItem);
-  }
+  if (browser) {
+    const localStorageItem = localStorage.getItem(localStorageName);
+    if (localStorageItem && localStorageItem !== 'undefined') {
+      try {
+        return JSON.parse(localStorageItem);
+      } catch {
+        return localStorageItem;
+      }
+    }
   }
 };
