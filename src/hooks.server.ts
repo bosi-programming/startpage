6742b09a6d9d16092c168c6e3dd05bdff393127e
Felipe Bosi <felipe.bosi@withclutch.com>
2025-03-19 17:20:37 -0300

    Add csp and security headers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,14 @@ import { TypeOrm } from "$lib/server/database";
 
 await TypeOrm.getDb();
 
+const securityHeaders = {
+  'Cross-Origin-Embedder-Policy': 'require-corp',
+  'Cross-Origin-Opener-Policy': 'same-origin',
+  'X-Content-Type-Options': 'nosniff',
+  'X-Frame-Options': 'DENY',
+  'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload',
+}
+
 import { redirect, type Handle } from '@sveltejs/kit';
 import { getUserIdFromCookies } from "./lib/server/utils/auth/cookies";
 import { userModel } from "./lib/server/models/User";
@@ -21,5 +29,8 @@ export const handle: Handle = async ({ event, resolve }) => {
   }
 
   const response = await resolve(event);
+  Object.entries(securityHeaders).forEach(
+    ([header, value]) => response.headers.set(header, value)
+  );
   return response;
 }
